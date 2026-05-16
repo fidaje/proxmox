b7356dc027eba8e1614a15d7afef1d1af3c2a5de
FRANCESCO IUORIO s359916 <s359916@studenti.polito.it>
2026-05-16 13:24:56 +0200

    refactor main.py to use asyncio for Proxmox API calls and improve error handling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import asyncio
 import proxmox
 
 from telegram import Update
@@ -12,7 +13,11 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-AUTHORIZED_USER_IDs = [int(os.getenv('AUTHORIZED_USER_ID')), int(os.getenv('GCC'))]
+try:
+    AUTHORIZED_USER_IDs = [int(os.getenv(key)) for key in os.environ if key.startswith('AUTHORIZED_USER_ID')]
+except ValueError:
+    logger.error("One or more AUTHORIZED_USER_ID values in the .env file are not valid.")
+    AUTHORIZED_USER_IDs = []
 TELEGRAM_CT = os.getenv('TELEGRAM_CT')
 
 
@@ -27,21 +32,40 @@ def authorized_user(func):
 
 @authorized_user
 async def status_all(update: Update, context) -> None:
-    await update.message.reply_text(proxmox.list_all())
+    try:
+        res = await asyncio.to_thread(proxmox.list_all)
+        await update.message.reply_text(res)
+    except Exception as e:
+        logger.error(f"Error occurred while fetching all status: {e}")
+        await update.message.reply_text("⚠️ An error occurred while fetching status.\nServer may be offline or there might be an issue with the Proxmox API.")
 
 @authorized_user
 async def status_containers(update: Update, context) -> None:
-    await update.message.reply_text(proxmox.list_containers())
+    try:
+        res = await asyncio.to_thread(proxmox.list_containers)
+        await update.message.reply_text(res)
+    except Exception as e:
+        logger.error(f"Error occurred while fetching container status: {e}")
+        await update.message.reply_text("⚠️ An error occurred while fetching container status.")
 
 @authorized_user
 async def status_vms(update: Update, context) -> None:
-    await update.message.reply_text(proxmox.list_vms())
+    try:
+        res = await asyncio.to_thread(proxmox.list_vms)
+        await update.message.reply_text(res)
+    except Exception as e:
+        logger.error(f"Error occurred while fetching VM status: {e}")
+        await update.message.reply_text("⚠️ An error occurred while fetching VM status.")
 
 
 @authorized_user
 async def summary(update: Update, context) -> None:
-    await update.message.reply_text(proxmox.summary())
-
+    try:
+        res = await asyncio.to_thread(proxmox.summary)
+        await update.message.reply_text(res)
+    except Exception as e:
+        logger.error(f"Error occurred while fetching system information: {e}")
+        await update.message.reply_text("⚠️ An error occurred while fetching system information.")
 
 @authorized_user
 async def change_status(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -49,15 +73,31 @@ async def change_status(update: Update, context: ContextTypes.DEFAULT_TYPE):
         vmid = ' '.join(context.args)
         if not vmid.isdigit():
             await update.message.reply_text("VMID must be a number")
+            return
         
         if vmid == TELEGRAM_CT:
             await update.message.reply_text("You cannot change the status of this container.")
             return
         
-        res = proxmox.stop_or_start(int(vmid))
-        await update.message.reply_text(f"Changing status of VMID: {vmid}\n{res}")
+        try:
+            res = await asyncio.to_thread(proxmox.stop_or_start, int(vmid))
+            await update.message.reply_text(f"Changing status of VMID: {vmid}\n{res}")
+        except Exception as e:
+            logger.error(f"Error occurred while changing status of VMID {vmid}: {e}")
+            await update.message.reply_text("⚠️ An error occurred while changing the status.")
     else:
-        await update.message.reply_text("Please specify a VMID to change its status.")
+        await update.message.reply_text("Please specify a VMID to change its status.\nE.g. /change 101")
+
+
+@authorized_user
+async def shutdown_system(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        res = await asyncio.to_thread(proxmox.shutdown_system)
+        await update.message.reply_text(res)
+    except Exception as e:
+        logger.error(f"Error occurred while shutting down the system: {e}")
+        await update.message.reply_text("⚠️ An error occurred while shutting down the system.")
+
 
 @authorized_user
 async def help(update: Update, context) -> None:
@@ -68,14 +108,11 @@ async def help(update: Update, context) -> None:
         "/vms - Show the status of virtual machines\n"
         "/info - Show system information\n"
         "/change <vmid> - Change the status of the specified virtual machine or container\n"
+        "/shutdown - Shut down the Proxmox host system\n"
         "/help - Show this help message \n"
     )
     await update.message.reply_text(help_text)
 
-@authorized_user
-async def shutdown_system(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    res = proxmox.shutdown_system()
-    await update.message.reply_text(res)
 
 def main() -> None:
     
@@ -92,7 +129,6 @@ def main() -> None:
 
 
     application.run_polling()
-
 
 
 
